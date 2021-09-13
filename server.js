c4f004871e56891cecf7aedce7fce34b861db323
unknown <costarebelo.tiago@gmail.com>
2021-09-13 17:55:14 +0100

    updating server

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const register = require('./controlers/register');
 const signin = require('./controlers/signin'); 
 const profile = require('./controlers/profile'); 
 const image = require('./controlers/image'); 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 
 const db = knex({
     client: 'pg',
